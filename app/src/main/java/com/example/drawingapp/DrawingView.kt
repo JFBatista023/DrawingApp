e3bf4f5ec134380fdc968b4f2c69164f645861cd
João Filipe <joaofilipeb045@gmail.com>
2023-09-28 12:46:37 -0300

    Adding DrawingView

---
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -0,0 +1,83 @@
+package com.example.drawingapp
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+    private lateinit var drawPath: FingerPath
+    private lateinit var canvasPaint: Paint
+    private lateinit var drawPaint: Paint
+    private var color = Color.BLACK
+    private lateinit var canvas: Canvas
+    private lateinit var canvasBitmap: Bitmap
+    private var brushSize: Float = 0.toFloat()
+
+    init {
+        setUpDrawing()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(canvasBitmap)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                drawPath.color = color
+                drawPath.brushThickness = brushSize.toFloat()
+
+                drawPath.reset()
+                drawPath.moveTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                drawPath.lineTo(touchX!!, touchY!!)
+            }
+
+            MotionEvent.ACTION_UP -> {
+                drawPath = FingerPath(color, brushSize)
+            }
+
+            else -> return false
+        }
+
+        invalidate()
+        return true
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        canvas?.drawBitmap(canvasBitmap, 0f, 0f, drawPaint)
+        if (!drawPath.isEmpty) {
+            drawPaint.strokeWidth = drawPath.brushThickness
+            drawPaint.color = drawPath.color
+            canvas?.drawPath(drawPath, drawPaint)
+        }
+    }
+
+    private fun setUpDrawing() {
+        drawPaint = Paint()
+        drawPath = FingerPath(color, brushSize)
+        drawPaint.color = color
+        drawPaint.style = Paint.Style.STROKE
+        drawPaint.strokeJoin = Paint.Join.ROUND
+        drawPaint.strokeCap = Paint.Cap.ROUND
+
+        canvasPaint = Paint(Paint.DITHER_FLAG)
+        brushSize = 20.toFloat()
+    }
+
+    internal inner class FingerPath(var color: Int, var brushThickness: Float): Path()
+}
